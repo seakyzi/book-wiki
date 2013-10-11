7545ab28c6223d62e7594157df5853cc4248729e
seakyzi <seakyzi1994@gmail.com>
2013-10-12 06:13:43 +0800

    email validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: {maximum:18}
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+){1,2}\z/i
   validates :email, presence: true, format:{with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
 
   private
